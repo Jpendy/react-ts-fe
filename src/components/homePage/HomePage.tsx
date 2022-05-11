7d2130224fddafcbb98cbb6181ec2f3487dcb6c4
Jpendy <Jpendy256@gmail.com>
2022-05-10 18:07:41 -0700

    added modal component and useViewport hook

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function HomePage() {
     return (
-        <div>HomePage</div>
+        <div style={{ height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '26px' }} >
+            Welcome to the Home Page!
+        </div>
     )
 }
